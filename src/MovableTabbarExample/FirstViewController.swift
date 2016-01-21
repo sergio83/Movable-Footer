99277ba1032f2f325f9034b0fff0a9a39e43c546
Sergio Cirasa <sergiocirasa@gmail.com>
2016-01-21 12:22:00 -0300

    Code updated to swift 2.0

--- src/MovableTabbarExample/FirstViewController.swift
+++ src/MovableTabbarExample/FirstViewController.swift
@@ -20,7 +20,7 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
     {
         super.viewDidLoad()
         self.title = "Test"
-        self.movableFooterController = MovableFooterController(footerView: self.tabBarController.tabBar ,scrollView: self.tableView,containerView: self.view);
+        self.movableFooterController = MovableFooterController(footerView: self.tabBarController!.tabBar ,scrollView: self.tableView,containerView: self.view);
     }
     
     override func viewWillAppear(animated: Bool)
@@ -47,35 +47,35 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
 // MARK: - UITableViewDataSource
 //------------------------------------------------------------------------------------------------------------------------------------------
-    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
             return 50
     }
     
-    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
             let kCellIdentifier: String = "CellIdentifier"
-            var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
-            cell.textLabel.text = "Cell  \(indexPath.row)"
+            let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier)! as UITableViewCell
+            cell.textLabel!.text = "Cell  \(indexPath.row)"
             return cell
     }
 // MARK: - UIScrollViewDelegate
 //------------------------------------------------------------------------------------------------------------------------------------------
-    func scrollViewWillBeginDragging(scrollView: UIScrollView!)
+    func scrollViewWillBeginDragging(scrollView: UIScrollView)
     {
         self.movableFooterController?.scrollViewWillBeginDragging(scrollView)
     }
     
-    func scrollViewWillEndDragging(scrollView: UIScrollView!, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+    func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
         self.movableFooterController?.scrollViewWillEndDragging(scrollView,withVelocity: velocity,targetContentOffset: targetContentOffset)
     }
     
-    func scrollViewDidScroll(scrollView: UIScrollView!)
+    func scrollViewDidScroll(scrollView: UIScrollView)
     {
         self.movableFooterController?.scrollViewDidScroll(scrollView)
     }
     
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView!)
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView)
     {
        self.movableFooterController?.scrollViewDidEndDecelerating(scrollView)
     }
